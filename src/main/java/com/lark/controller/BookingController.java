fad77da1291e083d10644676939a5399c832738e
lark <2397391125@qq.com>
2017-02-18 10:00:30 +0800

    remove unuse code

--- src/main/java/com/lark/controller/BookingController.java
+++ src/main/java/com/lark/controller/BookingController.java
@@ -10,7 +10,6 @@ import com.lark.service.BookingService;
 
 import com.lark.service.FutureBookingService;
 import com.lark.service.RoomService;
-import com.lark.utils.DateFormat;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
--- src/main/java/com/lark/service/UserService.java
+++ src/main/java/com/lark/service/UserService.java
@@ -1,10 +1,14 @@
 package com.lark.service;
 
+import com.lark.dao.mapper.UserMapper;
 import com.lark.dao.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by skylark on 2017/1/17.
  */
-public interface UserService {
-    User getUser(int userId);
+@Service
+public class UserService {
+
 }
